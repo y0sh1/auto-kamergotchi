ba9a638e1141c03129380ca76d07cc7268a5ba46
Jos <jos@dhcp-145.101.82.109.pool.hsleiden.nl>
2017-02-21 18:26:04 +0100

    Initial commit

---
+++ care.py
@@ -0,0 +1,35 @@
+import requests
+import configparser
+import json
+
+
+
+
+class Care:
+
+    token = None
+
+    headers = {'User-Agent': 'kamergotchi/86 CFNetwork/808.3 Darwin/16.3.0',
+               'x-player-token': '',
+               'content-type': 'application/json;charset=utf-8'}
+
+    def feed(self):
+        return self.post_to_api(json.dumps({"bar": "food"}))
+
+    def attention(self):
+        return self.post_to_api(json.dumps({"bar": "attention"}))
+
+    def knowledge(self):
+        return self.post_to_api(json.dumps({"bar": "knowledge"}))
+
+    def post_to_api(self, payload):
+        with requests.Session() as s:
+            r = s.post("https://api.kamergotchi.nl/game/care", headers=self.headers, verify=True, data=payload)
+            response = r.json()
+            remaining = response['game']['careLeft']
+            print(remaining)
+
+if __name__ in '__main__':
+    Game = Care()
+    Game.post_to_api(json.dumps({"bar": "food"}))
+
