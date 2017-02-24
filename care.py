1d0415fba4f4020b1c98eb3470eee3f994cbb2f7
y0sh1 <joshua@theinternets.nl>
2017-02-24 09:58:48 +0100

    little fuckup in  the timefield selection in the claming code

--- care.py
+++ care.py
@@ -47,9 +47,9 @@ class Care:
         response = self.game_info()
         if 'message' not in response:
             current_time = datetime.now()
-            last_reset_time = datetime.strptime(response['game']['careReset'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            last_reset_time = datetime.strptime(response['game']['claimReset'], "%Y-%m-%dT%H:%M:%S.%fZ")
             claim_diff = (current_time - last_reset_time).total_seconds()
-            if claim_diff >= 2700:
+            if claim_diff >= response['game']['claimLimitSeconds']:
                 self.claim_bonus()
                 print("We claimed a bonus")
 
